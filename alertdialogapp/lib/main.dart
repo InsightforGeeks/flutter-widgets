fe336b9cc45a6b8f82ce28784ba0bc17c42c5eb4
sobin007 <sobinmathew988@gmail.com>
2023-07-22 10:58:21 +0530

    new

--- alertdialogapp/lib/main.dart
+++ alertdialogapp/lib/main.dart
@@ -10,8 +10,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter For Geeks',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green.shade700),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.green.shade800, primary: Colors.green.shade700),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter For Geeks'),
@@ -33,7 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.primary,
         title: Text(widget.title),
       ),
       body: Center(
@@ -59,23 +61,26 @@ class _MyHomePageState extends State<MyHomePage> {
                               ))
                     },
                 child: const Text('Simple Alert Dialog')),
+            const SizedBox(
+              height: 20,
+            ),
             ElevatedButton(
                 onPressed: () => {
                       showDialog(
                           context: context,
                           builder: (ctx) => AlertDialog(
-                                title: Text("Delete File"),
-                                content: Text(
+                                title: const Text("Delete File"),
+                                content: const Text(
                                     "Are you sure you want to delete this file?"),
                                 actions: [
                                   TextButton(
-                                    child: Text("Cancel"),
+                                    child: const Text("Cancel"),
                                     onPressed: () {
                                       Navigator.of(context).pop();
                                     },
                                   ),
                                   TextButton(
-                                    child: Text("Delete"),
+                                    child: const Text("Delete"),
                                     onPressed: () {
                                       // Perform the delete action here
                                       Navigator.of(context).pop();
@@ -85,25 +90,28 @@ class _MyHomePageState extends State<MyHomePage> {
                               ))
                     },
                 child: const Text('Confirmation Dialog')),
+            const SizedBox(
+              height: 20,
+            ),
             ElevatedButton(
                 onPressed: () => {
                       showDialog(
                           context: context,
                           builder: (ctx) => AlertDialog(
-  title: Text("Enter your name"),
-  content: TextField(
-    // Controller and other TextField properties can be added here
-  ),
-  actions: [
-    TextButton(
-      child: Text("Submit"),
-      onPressed: () {
-        // Process the entered data here
-        Navigator.of(context).pop();
-      },
-    ),
-  ],
-)
+                                title: const Text("Enter your name"),
+                                content: const TextField(
+                                    // Controller and other TextField properties can be added here
+                                    ),
+                                actions: [
+                                  TextButton(
+                                    child: const Text("Submit"),
+                                    onPressed: () {
+                                      // Process the entered data here
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              ))
                     },
                 child: const Text('Input Dialog'))
           ],
